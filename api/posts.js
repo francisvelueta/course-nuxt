ba72ae4ffb9cbc43765afc2493a5b703a56f4215
Francis Velueta <fvelueta@outlook.com>
2019-07-12 14:17:58 -0500

    adding async data

---
+++ api/posts.js
@@ -0,0 +1,29 @@
+export const loadPosts = () => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve([
+        {
+          id: '1',
+          title: 'First Post',
+          previewText: 'This is our firts post',
+          thumbnail:
+            'https://d2v9y0dukr6mq2.cloudfront.net/video/thumbnail/4zx2oCU_lijl3z600/videoblocks-hackers-program-code-running-on-screen-4k_bwxci_4l_thumbnail-full01.png'
+        },
+        {
+          id: '2',
+          title: 'Second Post',
+          previewText: 'This is our second post',
+          thumbnail:
+            'https://concepto.de/wp-content/uploads/2015/03/software-1-e1550080097569.jpg'
+        },
+        {
+          id: '3',
+          title: 'Third Post',
+          previewText: 'This is our third post',
+          thumbnail:
+            'https://www.muycomputerpro.com/wp-content/uploads/2014/10/desarrollo_software.jpg'
+        }
+      ])
+    }, 1500)
+  })
+}
--- pages/index.vue
+++ pages/index.vue
@@ -3,41 +3,26 @@
     <section class="intro">
       <h1>Get the latest tech news!</h1>
     </section>
-    <PostList :posts="loadedPost" />
+    <PostList :posts="loadedPosts" />
   </div>
 </template>
 
 <script>
 import PostList from "@/components/Posts/PostList";
+
+import { loadPosts } from "@/api/posts";
 export default {
   components: {
     PostList
   },
-  data: () => ({
-    loadedPost: [
-      {
-        id: "1",
-        title: "First Post",
-        previewText: "This is our firts post",
-        thumbnail:
-          "https://d2v9y0dukr6mq2.cloudfront.net/video/thumbnail/4zx2oCU_lijl3z600/videoblocks-hackers-program-code-running-on-screen-4k_bwxci_4l_thumbnail-full01.png"
-      },
-      {
-        id: "2",
-        title: "Second Post",
-        previewText: "This is our second post",
-        thumbnail:
-          "https://concepto.de/wp-content/uploads/2015/03/software-1-e1550080097569.jpg"
-      },
-      {
-        id: "3",
-        title: "Third Post",
-        previewText: "This is our third post",
-        thumbnail:
-          "https://www.muycomputerpro.com/wp-content/uploads/2014/10/desarrollo_software.jpg"
-      }
-    ]
-  })
+  // Asyncdata (need it asynchronous data)with async await
+  asyncData: async context => {
+    const loadedPosts = await loadPosts();
+    if (!loadedPosts) return new Error();
+    return {
+      loadedPosts
+    };
+  }
 };
 </script>
 <style scoped>
