0a1acc77ac0c2e37533220d31df1c0596755c133
Francis Velueta <fvelueta@outlook.com>
2019-07-15 15:45:02 -0500

    storing token

--- pages/admin/auth/index.vue
+++ pages/admin/auth/index.vue
@@ -29,17 +29,15 @@ export default {
   },
   methods: {
     onSubmit() {
-      let authUrl = `https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=${process.env.fbApiKey}`;
-      if (!this.isLogin) {
-        authUrl = `https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=${process.env.fbApiKey}`;
-      }
-      this.$axios
-        .$post(authUrl, {
+      this.$store
+        .dispatch("auth/authenticateUser", {
+          isLogin: this.isLogin,
           email: this.email,
-          password: this.password,
-          returnSecureToken: true
+          password: this.password
         })
-        .then(res => console.log(res))
+        .then(() => {
+          this.$router.push("/admin");
+        })
         .catch(e => console.log(e));
     }
   }
---
+++ store/auth/index.js
@@ -0,0 +1,34 @@
+export const state = () => ({
+  token: null
+})
+
+export const getters = {}
+
+export const mutations = {
+  setToken(state, token) {
+    state.token = token
+  }
+}
+
+export const actions = {
+  authenticateUser(vxContext, authData) {
+    let authUrl = `https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=${
+      process.env.fbApiKey
+    }`
+    if (!authData.isLogin) {
+      authUrl = `https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=${
+        process.env.fbApiKey
+      }`
+    }
+    return this.$axios
+      .$post(authUrl, {
+        email: authData.email,
+        password: authData.password,
+        returnSecureToken: true
+      })
+      .then(res => {
+        vxContext.commit('setToken', res.idToken)
+      })
+      .catch(e => console.log(e))
+  }
+}
